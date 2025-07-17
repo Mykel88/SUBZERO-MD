a0f7153ec8c1a35736e33b805bf857cad405b6d8
Mykel88 <michealolawale0802@gmail.com>
2025-07-17 23:05:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~478b22743c69e2e421ab6233e8580758b79d7216", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
