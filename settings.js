18888cebf1fa8518a0b6416b0740329423e89d69
Mykel88 <michealolawale0802@gmail.com>
2025-07-18 15:18:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~478b22743c69e2e421ab6233e8580758b79d7216", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD~8ShAUK3P0ZPcWtoFlL0H", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2349165331115", // Put Owner Number Here 
+  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
